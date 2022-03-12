0ab4f69c312cedb070ade33224369b0fe8def078
Deivid Souza <ddeividd.eu@hotmail.com>
2022-03-12 20:02:07 -0300

    Reestruturação das pastas; exportando com Default

--- 1_fundamentos/projeto/myskills/App.js
+++ 1_fundamentos/projeto/myskills/App.js
@@ -1,30 +1,7 @@
-//React é obrigatório para construirmos interfaces
 import React from 'react';
 
-//trazemos do React o que precisamos no contexto mobile
-import { View, Text } from 'react-native';
+import Home from './src/pages/Home'
 
 export default function App() {
-  return (
-
-    //Nova forma de uso do Fragment (serve para agrupar, não tem impacto nenhum visualmente)
-    <>
-      <View style={{
-        //tomará toda a tela
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <Text>React Native</Text>
-      </View>
-
-      <Text style={{
-
-        alignSelf: 'center',
-        marginBottom: 20
-
-      }}>Ignite</Text>
-    </>
-
-  )
+  return <Home />
 }
