ec49ed93ac3195f4a077b69dbf887b131276dfc0
Deivid Souza <ddeividd.eu@hotmail.com>
2022-03-12 20:05:14 -0300

    Exportando sem Default

--- 1_fundamentos/projeto/myskills/src/pages/Home.js
+++ 1_fundamentos/projeto/myskills/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Home() {
+export function Home() {
     return (
         <View style={{
             flex: 1,
