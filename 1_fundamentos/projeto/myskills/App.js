ff57b0a5e29216618f4be4a3fc03f99673f4ddb5
Deivid Souza <ddeividd.eu@hotmail.com>
2022-03-12 19:40:23 -0300

    Uma forma de utilizar o Fragment

--- 1_fundamentos/projeto/myskills/App.js
+++ 1_fundamentos/projeto/myskills/App.js
@@ -1,15 +1,30 @@
-import React from 'react';                 //para construirmos interfaces
-import { View, Text } from 'react-native'; //trazemos do React o que precisamos no contexto mobile
+//React é obrigatório para construirmos interfaces
+//Fragment é como uma View, serve para agrupar (não tem impacto nenhum visualmente)
+import React, { Fragment } from 'react';
+
+//trazemos do React o que precisamos no contexto mobile
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,  //tomará toda a tela
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>React Native</Text>
-      <Text>Ignite</Text>
-    </View>
+
+    <Fragment>
+      <View style={{
+        //tomará toda a tela
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>React Native</Text>
+      </View>
+
+      <Text style={{
+
+        alignSelf: 'center',
+        marginBottom: 20
+
+      }}>Ignite</Text>
+    </Fragment>
+
   )
 }
